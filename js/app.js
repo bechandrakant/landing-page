497d39e511be8ff1b021f3634d295beee6d4224e
Chandrakant <chandrakantsavvy@gmail.com>
2020-01-13 23:33:08 +0530

    fixed scrolling issues

--- js/app.js
+++ js/app.js
@@ -9,14 +9,13 @@ function addListItem(label, targetLink) {
   fragment.appendChild(listItem);
 }
 
-for (let i = 1; i < 5; i++) {
+// add 4 navigation list items
+for (let i = 1; i <= 4; i++) {
   addListItem("Section" + i, "#section-" + i);
 }
 
 const navLinks = document.getElementById("nav-list");
 navLinks.appendChild(fragment);
-
-
 
 //Change navigation style on scroll
 window.addEventListener('scroll', event => {
@@ -28,15 +27,22 @@ window.addEventListener('scroll', event => {
 //toggle active navigation on scroll
 window.addEventListener('scroll', event => {
   let navigationLinks = document.querySelectorAll('nav ul li a');
-  let distanceFromTop = window.scrollY;
 
   navigationLinks.forEach(link => {
     let section = document.querySelector(link.hash);
 
-    if (
-      section.offsetTop <= distanceFromTop &&
-      section.offsetTop + section.offsetHeight > distanceFromTop
-    ) {
+    let sectionTopPosition = section.offsetTop;
+    let sectionHeight = section.offsetHeight;
+
+    // padding value is added to adjust frame due to padding
+    // 50 and 90 are padding top value on large and small screen
+    let padding = (window.innerWidth > 500) ? 50 : 90;
+    let scrollPosition = window.scrollY + padding;
+
+    // If section is in frame (check using scrollPosition)
+    // frame is rectangular box from section top position to section top position plus height of section
+    if (sectionTopPosition <= scrollPosition &&
+      sectionTopPosition + sectionHeight >= scrollPosition) {
       link.classList.add('active');
     } else {
       link.classList.remove('active');
